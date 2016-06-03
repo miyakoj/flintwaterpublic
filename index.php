7a384574d1f4fcd515fcda7db564532b167a396c
miyakoj <miyakoj@umflint.edu>
2016-06-03 19:18:46 -0400

    Updated to fix database connectivity

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 require_once "includes/template.class";
 require_once "includes/queries.php";
 
-$page = new webpageTemplate("includes/template.inc");
-$page->set("MAP_POPULATION", "<script>test</script>");
+$result = queries("lead");
+
+/*while ($row = $result->fetch_assoc()) {
+	print_r($row);
+}*/
+
+$data = "<script></script>";
+
+$page = new webpageTemplate("includes/template.html");
+$page->set("MAP_POPULATION", $data);
 $page->create();
