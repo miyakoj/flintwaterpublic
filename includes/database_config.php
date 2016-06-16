a6f09c0c8d6c0fff92e6e173997668a4975884f5
miyakoj <miyakoj@umflint.edu>
2016-06-16 15:40:25 -0400

    Adding provider database connectivity

--- includes/database_config.php
+++ includes/database_config.php
@@ -1,14 +1,17 @@
 <?php
 
-$mysqli = new mysqli(
+/*$mysqli = new mysqli(
 	null, // host
 	getenv('MYSQL_USER'), // username
 	getenv('MYSQL_PASSWORD'), // password
 	'waterdata', // database name
 	null,
-	getenv('MYSQL_INSTANCE')
-);
+	getenv('MYSQL_INSTANCE') // database instance
+);*/
+
+$mysqli = new mysqli("localhost", "root", "k1ch1r1K4ch1r1", "waterdata");
 
 if ($mysqli->connect_errno) {
-	print "error";
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
 }
