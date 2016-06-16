b7197e182cb24d60f12600236e444bbab2b30c16
miyakoj <miyakoj@umflint.edu>
2016-06-16 15:41:09 -0400

    Revert "Adding provider database connectivity"
    This reverts commit 911326ba7c305eb0402c14e1eb696aa50636d715.

--- includes/database_config.php
+++ includes/database_config.php
@@ -1,17 +1,14 @@
 <?php
 
-/*$mysqli = new mysqli(
+$mysqli = new mysqli(
 	null, // host
 	getenv('MYSQL_USER'), // username
 	getenv('MYSQL_PASSWORD'), // password
 	'waterdata', // database name
 	null,
-	getenv('MYSQL_INSTANCE') // database instance
-);*/
-
-$mysqli = new mysqli("localhost", "root", "k1ch1r1K4ch1r1", "waterdata");
+	getenv('MYSQL_INSTANCE')
+);
 
 if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
+	print "error";
 }
