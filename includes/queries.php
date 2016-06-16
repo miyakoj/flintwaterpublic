73a6178f74273955a26a76fa56141a3dae420685
miyakoj <miyakoj@umflint.edu>
2016-06-16 15:41:09 -0400

    Revert "Adding provider database connectivity"
    This reverts commit 911326ba7c305eb0402c14e1eb696aa50636d715.

--- includes/queries.php
+++ includes/queries.php
@@ -2,21 +2,18 @@
 
 require_once "database_config.php";
 
-function queries($choice, $var = "") {
+function queries($choice, $var="") {
 	global $mysqli;
 	$choice = $mysqli->real_escape_string($choice);
 	
 	if (strcmp($choice, "lead") === 0)
 		$query = "SELECT * FROM `waterCondition` ORDER BY `leadLevel` ASC";
 	else if (strcmp($choice, "providers") === 0)
-		if (strcmp($var, "") === 0)		
-			$query = "SELECT locationName, AidLocation.aidAddress, hours, phone, city, zipcode, resType, notes, latitude, longitude FROM AidLocation, ResourcesQuantity JOIN GeoLocation ON REPLACE(aidAddress,'\r','') = REPLACE(StAddress,'\r','') WHERE AidLocation.aidAddress = ResourcesQuantity.aidAddress";
-		else
-			$query = "SELECT resType FROM `resourcesquantity` WHERE aidAddress = '" . $var . "'";
-		
-		//$query = "SELECT locationName, AidLocation.aidAddress, hours, phone, city, zipcode, resType, notes, latitude, longitude FROM AidLocation, ResourcesQuantity JOIN GeoLocation ON aidAddress = REPLACE(StAddress,'\r','') WHERE AidLocation.aidAddress = ResourcesQuantity.aidAddress";
-		//$query = "SELECT locationName, AidLocation.aidAddress, hours, phone, city, zipcode, resType, notes, latitude, longitude FROM AidLocation, ResourcesQuantity JOIN GeoLocation ON REPLACE(aidAddress,'\r','') = REPLACE(StAddress,'\r','') WHERE AidLocation.aidAddress = ResourcesQuantity.aidAddress AND AidLocation.aidAddress = " . $var;
-		//else
+		$query = "SELECT locationName, AidLocation.aidAddress, hours, phone, city, zipcode, resType, notes, latitude, longitude FROM AidLocation, ResourcesQuantity JOIN GeoLocation ON aidAddress = REPLACE(StAddress,'\r','') WHERE AidLocation.aidAddress = ResourcesQuantity.aidAddress";
+	
+	
+
+SELECT locationName, AidLocation.aidAddress, hours, phone, city, zipcode, resType, notes, latitude, longitude FROM AidLocation, ResourcesQuantity JOIN GeoLocation ON REPLACE(aidAddress,'\r','') = REPLACE(StAddress,'\r','') WHERE AidLocation.aidAddress = ResourcesQuantity.aidAddress AND AidLocation.aidAddress = <insert your variable here>;
 
 
 	
