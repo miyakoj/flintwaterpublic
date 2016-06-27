6e8b5449ecff00e11c9fe2369bc829cb29880ba8
philb <philboyd72@gmail.com>
2016-06-27 15:26:06 -0400

    Alerts and News to Material Design

--- js/alerts.js
+++ js/alerts.js
@@ -11,8 +11,8 @@ function onStartup() {
 		   for (i in result.feed.entries) {
 			   var entry = result.feed.entries[i];
 			   
-			   html += '<div class="panel panel-default">';
-			   html += '<div class="panel-body"><a href="' + entry.link +'">' + entry.title + '</a> (' + entry.publishedDate + ')</div>';
+			   html += '<div class="card">';
+			   html += '<div class="card-inner"><a href="' + entry.link +'">' + entry.title + '</a> <div>' + entry.publishedDate + '</div></div>';
 			   html += '</div>';
 		   }
 		   
