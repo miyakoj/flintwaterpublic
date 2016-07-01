15d7d509d20ad501bc4603a356a2cb76266e9608
paulstpierre <pstpierr@umflint.edu>
2016-07-01 12:04:24 -0400

    News Page changed

--- js/news.js
+++ js/news.js
@@ -2,23 +2,60 @@ google.load("feeds", "1");
 google.setOnLoadCallback(onStartup);
 
 function onStartup() {
-	var query = 'site:mlive.com Flint water';
-    google.feeds.findFeeds(query, findEntries);
+	mLive();
+	michiganRadio();
 }
 
-function findEntries(result) {
-   if (!result.error) {
-	   var html = '';
-	   
-	   for (var i = 0; i < result.entries.length; i++) {
-		   var entry = result.entries[i];
-		   
-		   html += '<div class="panel panel-default">';
-		   html += '<div class="panel-heading"><a href="' + entry.link +'">' + entry.title + '</a></div>';
-		   html += '<div class="panel-body">' + entry.contentSnippet + '</div>';
-		   html += '</div>';
+function mLive() {
+	var feed = new google.feeds.Feed("http://blog.mlive.com/newsnow_impact/atom.xml");
+	feed.setNumEntries(50);
+	feed.load(function(result) {
+	   if (!result.error) {
+		   var html = '';
+			result.feed.entries.sort(function (a,b){
+			return new Date(b.publishedDate) - new Date(a.publishedDate);
+			});
+	   for (var i = 0; i < result.feed.entries.length; i++) {
+		   var entry = result.feed.entries[i];
+			if(entry.categories.indexOf("tag:flint-water") != -1){
+				html += '<div class="panel panel-default">';
+				html += '<div class="panel-heading"><img class="pull-left" src="../images/Mlive.jpg" alt="Mlive">' + '<span>Mlive</span>' + '<h5 class="text-right">' + date(entry.publishedDate) + '</h5></div>';
+				html += '<div class="panel-body"><a href="' + entry.link +'">' + entry.title + " " + '</a><p>' + entry.contentSnippet + '</p></div>';
+				html += '</div>';
+			}
 	   }
-	   
 	   $("#news").html(html);
-   }
+		
+		}	
+	})
 }
+
+
+function michiganRadio()  {
+	var feed = new google.feeds.Feed("http://michiganradio.org/rss.xml");
+	feed.setNumEntries(50);
+	feed.load(function(result) {
+	   if (!result.error) {
+		   var html = '';
+			result.feed.entries.sort(function (a,b){
+			return new Date(b.publishedDate) - new Date(a.publishedDate);
+			});
+	   for (var i = 0; i < result.feed.entries.length; i++) {
+		   var entry = result.feed.entries[i];
+			if(entry.title.search(/(Flint)+(')*[\w\s\d]+(water)*/) > 0){
+				html += '<div class="panel panel-default">';
+				html += '<div class="panel-heading"><img class="pull-left" src="../images/michigan_radio.png" alt="Michigan radio">' + '<span>Michigan Radio</span>' + '<h5 class="text-right">' + date(entry.publishedDate) + '</h5></div>';
+				html += '<div class="panel-body"><a href="' + entry.link +'">' + entry.title + " " + '</a><p>' + entry.contentSnippet + '</p></div>';
+				html += '</div>';
+			}
+	   }
+	   $("#news").append(html);
+		
+		}	
+	})
+}
+
+function date(input) {
+	return input.substring(0,16);
+}
+
---
+++ js/toggle_scrollbar.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+var windowWidth = window.innerWidth;
+var hidWidth;
+var scrollBarWidths = 40;
+var numItems = 8;
+var placeCounter = 0;
+
+var lengthOfScroll = function(){
+	return $('.list label').outerWidth();
+};
+
+var widthOfList = function(){
+    var itemsWidth = 0;
+  $('.list label').each(function(){
+    var itemWidth = $(this).outerWidth();
+    itemsWidth+=itemWidth;
+  });
+  return itemsWidth;
+};
+
+$('.list label').css('numItems', windowWidth/numItems);
+
+var widthOfHidden = function(){
+  return (($('.wrapper').outerWidth())-widthOfList()-getLeftPosi())-scrollBarWidths;
+};
+
+var getLeftPosi = function(){
+  return $('.list').position().left;
+};
+
+var reAdjust = function(){
+  if (($('.wrapper').outerWidth()) < widthOfList()) {
+    $('.scroller-right').show();
+  }
+  else {
+    $('.scroller-right').hide();
+  }
+  
+  if (getLeftPosi()<0) {
+    $('.scroller-left').show();
+  }
+  else {
+    $('.item').animate({left:"-="+getLeftPosi()+"px"},'slow');
+  	$('.scroller-left').hide();
+  }
+}
+
+reAdjust();
+
+$(window).on('resize',function(e){ 
+  	reAdjust();
+});
+
+$('.scroller-right').click(function() {
+ if (placeCounter < widthOfList()){
+  $('.scroller-left').fadeIn('slow');
+  $('.scroller-right').fadeIn('slow');
+  placeCounter += ($('.wrapper').outerWidth()-scrollBarWidths);
+  $('.list').animate({left:"-="+lengthOfScroll()+"px"},'slow',function(){
+  });
+  }
+  else {
+	$('.scroller-left').fadeIn('slow');
+	$('.scroller-right').fadeOut('slow');
+
+	$('.list').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){
+	});
+	}
+  });
+
+$('.scroller-left').click(function() {
+
+  if (placeCounter!==0) {
+	$('.scroller-right').fadeIn('slow');
+	$('.scroller-left').fadeIn('slow');
+    placeCounter -= ($('.wrapper').outerWidth()-scrollBarWidths);
+
+  	$('.list').animate({left:"+="+lengthOfScroll()+"px"},'slow', function(){		
+	});
+  }
+    else {
+	$('.scroller-right').fadeIn('slow');
+	$('.scroller-left').fadeOut('slow');
+
+	$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){
+  	});
+  }
+});  
+});
