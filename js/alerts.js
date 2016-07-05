35379269585f4449786502af72c924ee141754b5
PatB93 <pbodell@umflint.edu>
2016-07-05 15:46:35 -0400

    Update Alerts
    Now Pulls for SQL database

--- js/alerts.js
+++ js/alerts.js
@@ -2,21 +2,26 @@ google.load("feeds", "1");
 google.setOnLoadCallback(onStartup);
 
 function onStartup() {
-	var feed = new google.feeds.Feed("http://www.michigan.gov/rss/0,2348,7-345-75303--91359-,00.xml");
-    feed.load(function(result) {
-	   if (!result.error) {		   
-		   var i;
-		   var html = '';
-		   
-		   for (i in result.feed.entries) {
-			   var entry = result.feed.entries[i];
-			   
-			   html += '<div class="card">';
-			   html += '<div class="card-inner"><a href="' + entry.link +'">' + entry.title + '</a> <div>' + entry.publishedDate + '</div></div>';
-			   html += '</div>';
-		   }
-		   
-		   $("#alerts").html(html);
+	var oReq = new XMLHttpRequest(); //New request object
+	oReq.open("get", "alertDataAccess.php", true); //Access my PHP file for query
+	oReq.send();   //Send request
+	var jsonFeed;
+    var i = 0;
+    var html = '';
+	oReq.onload = function() {
+		//This is where you handle what to do with the response from the SQL query.
+		//The actual data is found on this.responseText
+		jsonFeed = JSON.parse(this.responseText); 
+	   while(i < jsonFeed.length - 1) {			//output all of the alerts found for a given query (currently all alerts in database) HTML
+		   i += 1;
+		   html += '<div class="card">';
+		   html += '<div class="card-inner">'
+						+'<li> '+ jsonFeed[i].alertType.toString() +'</li>'
+						+'<div>'+ jsonFeed[i].description.toString()+'</div>'
+						+'<div>' + jsonFeed[i].alertDate.toString() + '</div></div>';
+		   html += '</div>';
 	   }
-	});
+	   $("#alerts").html(html);
+	};
+
 }
