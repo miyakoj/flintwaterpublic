ea25413a81914dabd068dfb187c230ddf0881dd9
Miyako <mystc.raine@gmail.com>
2016-10-05 20:46:52 -0400

    Updated location cards, IE detection

--- includes/fusion_table_updates.php
+++ includes/fusion_table_updates.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * Update the fusion tables if new water tests are found.
+ */
+
 include_once "vendor/autoload.php";
 
 $api_key = getenv('API_KEY');
@@ -9,6 +13,7 @@ $scopes = array(
         );
 $fusion_table_all = "17nXjYNo-XHrHiJm9oohgxBSyIXsYeXqlnVHnVrrX";
 $fusion_table_recent = "11sVcd8gCuqrM3H3UFViwXthNir3bUFc0BDwSJgoy";
+$fusion_table_test = "1hzW6T-v5Ak3KsAPrtXWfblTYd6tr4tt_FUxGCCiL";
 
 $client = new Google_Client();
 //$client->setHttpClient(new GuzzleHttp\Client(['verify' => '../vendor/ca-bundle.crt']));
@@ -24,7 +29,7 @@ $client->setRedirectUri($redirect_uri);
 /* Call the Fusion Table service. */
 $service = new Google_Service_Fusiontables($client);
 
-$result = $service->query->sql("SELECT Address FROM " . $fusion_table_all . " LIMIT 10");
+$result = $service->query->sql("SELECT * FROM " . $fusion_table_all . " LIMIT 10");
 
 var_dump($result);
 
